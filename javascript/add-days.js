70f43ca07d32dea0965131590e725f869206ab9b
Hamza Hamidi <hamidihamzacg@gmail.com>
2019-10-01 21:54:27 +0200

    Update add-days.js

--- javascript/add-days.js
+++ javascript/add-days.js
@@ -1,5 +1,5 @@
 function addDays(date, days) {
   const input = new Date(date);
-  const output = input.setDate(result.getDate() + days);
-  return output;
+  const output = input.setDate(input.getDate() + days);
+  return new Date(output);
 }
